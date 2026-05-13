b998fbe014d37545d3e3dd2b35ef397c99eba84f
jg <joao.gabriel.hermida@gmail.com>
2026-05-13 09:23:28 -0300

    Site c#

---
+++ ConsolesEJogosJoaoGabApp/Pages/QuemSomos.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ConsolesEJogosJoaoGabApp.Pages
+{
+    public class QuemSomosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
